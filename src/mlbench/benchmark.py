5a527c87fdab80870886e04bb19a1f05441b80c2
Nicholas Junge <nicholas.junge@web.de>
2024-01-11 15:18:45 +0100

    Add benchmark class stub
    The base object for benchmarking workloads. At this stage, basically a
    thin wrapper around a function.

---
+++ src/mlbench/benchmark.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Any, Callable
+
+
+@dataclass(frozen=True)
+class Benchmark:
+    """
+    Data model representing a benchmark function. Subclass this to define your own custom benchmark class.
+    """
+
+    fn: Callable
+    name: str
+    params: dict[str, Any]
+    setUp: Callable | None = None
+    tearDown: Callable | None = None
