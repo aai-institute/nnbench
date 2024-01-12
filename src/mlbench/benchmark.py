0866f3def4995920fb389c0fe1c7300df4d3fb27
Nicholas Junge <nicholas.junge@web.de>
2024-01-12 11:54:33 +0100

    Add tags to benchmark class, set up name in postinit hook
    This way, users can still specify a name without needing to set the
    unique params concatenation name all on their own.

--- src/mlbench/benchmark.py
+++ src/mlbench/benchmark.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Any, Callable
 
 
@@ -13,7 +13,21 @@ class Benchmark:
     """
 
     fn: Callable
-    name: str
-    params: dict[str, Any]
-    setUp: Callable | None = None
-    tearDown: Callable | None = None
+    name: str | None = field(default=None)
+    params: dict[str, Any] = field(repr=False, default_factory=dict)
+    """Default parameters to attach to a benchmark. Must match the benchmark's interface."""
+    setUp: Callable | None = field(repr=False, default=None)
+    """A setup hook run before the benchmark. Takes `params` as its only input."""
+    tearDown: Callable | None = field(repr=False, default=None)
+    """A teardown hook run after the benchmark. Takes `params` as its only input."""
+    tags: tuple[str, ...] = field(repr=False, default=())
+    """Additional tags to attach to the benchmark for bookkeeping and selective filtering during runs."""
+
+    def __post_init__(self):
+        if not self.name:
+            name = self.fn.__name__
+            if self.params:
+                name += "_" + "_".join(f"{k}={v}" for k, v in self.params.items())
+
+            super().__setattr__("name", name)
+        # TODO: Parse interface using `inspect`, attach to the class
