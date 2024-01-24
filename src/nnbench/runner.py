5b829f24557b88ae876d833313c18d88d3c5f766
Max Mynter <maxmynter@me.com>
2024-01-24 13:26:13 +0100

    Revert "Factor out orig_type"
    This reverts commit bab9b63d08a2135105369039b1fb2501f827625c.

--- src/nnbench/runner.py
+++ src/nnbench/runner.py
@@ -29,8 +29,8 @@ def _check(params: dict[str, Any], benchmarks: list[Benchmark]) -> None:
                     f"Found untyped parameter {name!r} in benchmark {bm.fn.__name__!r}."
                 )
 
-            orig_type = benchmark_interface[name].annotation
-            if name in benchmark_interface and orig_type != param_type:
+            if name in benchmark_interface and benchmark_interface[name].annotation != param_type:
+                orig_type = benchmark_interface[name].annotation
                 raise TypeError(
                     f"got non-unique types {orig_type}, {param_type} for parameter {name!r}"
                 )
