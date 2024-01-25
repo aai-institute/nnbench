f7523f39bf75d55f1e918a6dbe0129b4d3c99f90
Max Mynter <maxmynter@me.com>
2024-01-25 15:02:36 +0100

    rename hello.py in tests to be more descriptive

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -6,7 +6,7 @@ from nnbench.runner import AbstractBenchmarkRunner
 def test_runner_discovery(testfolder: str) -> None:
     r = AbstractBenchmarkRunner()
 
-    r.collect(os.path.join(testfolder, "hello.py"))
+    r.collect(os.path.join(testfolder, "simple_benchmark.py"))
     assert len(r.benchmarks) == 1
 
     r.clear()
