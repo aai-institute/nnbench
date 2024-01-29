7ad74a53452b224996df0c92038adbddbf3af34b
Max Mynter <maxmynter@me.com>
2024-01-29 17:08:09 +0100

    Add Tests

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -13,3 +13,21 @@ def test_runner_discovery(testfolder: str) -> None:
 
     r.collect(testfolder, tags=("runner-collect",))
     assert len(r.benchmarks) == 1
+
+
+def test_tag_selection(testfolder: str) -> None:
+    PATH = os.path.join(testfolder, "tag_selection_benchmark.py")
+
+    r = BenchmarkRunner()
+
+    r.collect(PATH, tags=())
+    assert len(r.benchmarks) == 3
+    r.clear()
+
+    r.collect(PATH, tags=("tag1",))
+    assert len(r.benchmarks) == 2
+    r.clear()
+
+    r.collect(PATH, tags=("tag2",))
+    assert len(r.benchmarks) == 1
+    r.clear()
