0ed924aac49c60591d59293c2a2e4594831c7dcd
Max Mynter <maxmynter@me.com>
2024-02-01 17:14:50 +0100

    Define abstract BenchmarkReporter Interface
    It's `report_result()` method is to be overridden by derived classes.
    The `report()` methods consumes a single or multiple `BenchmarkResult`s.
    Depending on the value of the `merge` attribute the results are reported individually or merged before the report.

--- src/nnbench/reporter.py
+++ src/nnbench/reporter.py
@@ -6,39 +6,40 @@ from __future__ import annotations
 import importlib
 import sys
 import types
-from typing import Any
 
 from nnbench.types import BenchmarkResult
 
 
-class BaseReporter:
+# TODO: Add IO mixins for database, file, and HTTP IO
+class BenchmarkReporter:
     """
     The base interface for a benchmark reporter class.
 
-    A benchmark reporter consumes benchmark results from a run, and subsequently
-    reports them in the way specified by the respective implementation's `report()`
+    A benchmark reporter consumes benchmark results from a previous run, and subsequently
+    reports them in the way specified by the respective implementation's `report_result()`
     method.
 
     For example, to write benchmark results to a database, you could save the credentials
-    for authentication in the class constructor, and then stream the results directly to
-    the database in `report()`, with preprocessing if necessary.
-
-    Parameters
-    ----------
-    **kwargs: Any
-        Additional keyword arguments, for compatibility with subclass interfaces.
+    for authentication on the class, and then stream the results directly to
+    the database in `report_result()`, with preprocessing if necessary.
     """
 
-    def __init__(self, **kwargs: Any):
-        pass
+    merge: bool = False
+    """Whether to merge multiple records before reporting."""
 
-    def report(self, result: BenchmarkResult) -> None:
+    def report_result(self, result: BenchmarkResult) -> None:
         raise NotImplementedError
 
+    def report(self, *results: BenchmarkResult) -> None:
+        if self.merge:
+            raise NotImplementedError
+        for res in results:
+            self.report_result(res)
 
-class ConsoleReporter(BaseReporter):
+
+class ConsoleReporter(BenchmarkReporter):
     # TODO: Implement regex filters, context values, display options, ... (__init__)
-    def report(self, result: BenchmarkResult) -> None:
+    def report_result(self, result: BenchmarkResult) -> None:
         try:
             from tabulate import tabulate
         except ModuleNotFoundError:
@@ -52,17 +53,17 @@ class ConsoleReporter(BaseReporter):
 
 
 # internal, mutable
-_reporter_registry: dict[str, type[BaseReporter]] = {
+_reporter_registry: dict[str, type[BenchmarkReporter]] = {
     "console": ConsoleReporter,
 }
 
 # external, immutable
-reporter_registry: types.MappingProxyType[str, type[BaseReporter]] = types.MappingProxyType(
+reporter_registry: types.MappingProxyType[str, type[BenchmarkReporter]] = types.MappingProxyType(
     _reporter_registry
 )
 
 
-def register_reporter(key: str, cls_or_name: str | type[BaseReporter]) -> None:
+def register_reporter(key: str, cls_or_name: str | type[BenchmarkReporter]) -> None:
     """
     Register a reporter class by its fully qualified module path.
 
@@ -71,7 +72,7 @@ def register_reporter(key: str, cls_or_name: str | type[BaseReporter]) -> None:
     key: str
         The key to register the reporter under. Subsequently, this key can be used in place
         of reporter classes in code.
-    cls_or_name: str | type[BaseReporter]
+    cls_or_name: str | type[BenchmarkReporter]
         Name of or full module path to the reporter class. For example, when registering a class
         ``MyReporter`` located in ``my_module``, ``name`` should be ``my_module.MyReporter``.
     """
