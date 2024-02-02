56f1482d6686cb5916ad93181754c3ef3be4e4fd
Max Mynter <maxmynter@me.com>
2024-02-02 15:03:10 +0100

    Refactor decorator tests

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -3,6 +3,8 @@ import pytest
 import nnbench
 from nnbench import benchmark, parametrize, product
 
+from .test_utils import has_expected_args
+
 
 def test_benchmark_no_args():
     @benchmark
@@ -23,13 +25,13 @@ def test_benchmark_with_args():
 
 def test_parametrize():
     @parametrize([{"param": 1}, {"param": 2}])
-    def parametrized_benchmark(param: str) -> str:
+    def parametrized_benchmark(param: int) -> int:
         return param
 
     assert len(parametrized_benchmark) == 2
-    assert parametrized_benchmark[0].fn.keywords["param"] == 1  # type: ignore
+    assert has_expected_args(parametrized_benchmark[0].fn, {"param": 1})
     assert parametrized_benchmark[0].fn() == 1
-    assert parametrized_benchmark[1].fn.keywords["param"] == 2  # type: ignore
+    assert has_expected_args(parametrized_benchmark[1].fn, {"param": 2})
     assert parametrized_benchmark[1].fn() == 2
 
 
@@ -47,13 +49,13 @@ def test_product():
         return iter1, iter2
 
     assert len(product_benchmark) == 4
-    assert product_benchmark[0].fn.keywords == {"iter1": 1, "iter2": "a"}  # type: ignore
+    assert has_expected_args(product_benchmark[0].fn, {"iter1": 1, "iter2": "a"})
     assert product_benchmark[0].fn() == (1, "a")
-    assert product_benchmark[1].fn.keywords == {"iter1": 1, "iter2": "b"}  # type: ignore
+    assert has_expected_args(product_benchmark[1].fn, {"iter1": 1, "iter2": "b"})
     assert product_benchmark[1].fn() == (1, "b")
-    assert product_benchmark[2].fn.keywords == {"iter1": 2, "iter2": "a"}  # type: ignore
+    assert has_expected_args(product_benchmark[2].fn, {"iter1": 2, "iter2": "a"})
     assert product_benchmark[2].fn() == (2, "a")
-    assert product_benchmark[3].fn.keywords == {"iter1": 2, "iter2": "b"}  # type: ignore
+    assert has_expected_args(product_benchmark[3].fn, {"iter1": 2, "iter2": "b"})
     assert product_benchmark[3].fn() == (2, "b")
 
 
