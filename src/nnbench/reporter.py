a9956f0d51839271e973b1e53d658866188a078e
Nicholas Junge <n.junge@appliedai.de>
2024-02-07 20:31:53 +0100

    Add selective printing and filtering functionality to console reporter
    Contains null-column filtering, a table format setter, a benchmark regex
    filter, and a context filter able to operate on nested values. This is
    especially important now that we pushed semantically related contexts
    one level up.

--- src/nnbench/reporter.py
+++ src/nnbench/reporter.py
@@ -3,11 +3,43 @@ A lightweight interface for refining, displaying, and streaming benchmark result
 """
 from __future__ import annotations
 
+import copy
 import importlib
+import re
 import sys
 import types
+from typing import Any
 
 from nnbench.types import BenchmarkRecord
+
+
+def nullcols(_benchmarks: list[dict[str, Any]]) -> set[str]:
+    nulls = set()
+    for i, bm in enumerate(_benchmarks):
+        bm_nulls = set(k for k, v in bm.items() if not v)
+
+        if i == 0:
+            # NB: This breaks if the schema is unstable.
+            nulls = bm_nulls
+        else:
+            # intersection to drop nulls that are populated in later benchmarks.
+            nulls &= bm_nulls
+    return nulls
+
+
+def nested_getitem(obj: dict[str, Any], item: str) -> Any:
+    items = item.split(".")
+    for n, it in enumerate(items):
+        try:
+            obj = obj[it]
+        except KeyError:
+            if n == 0:
+                msg = f"context has no member {item!r}"
+            else:
+                val = ".".join(items[: n + 1])
+                msg = f"nested context value {val!r} has no member {it!r}"
+            raise KeyError(msg) from None
+    return obj
 
 
 # TODO: Add IO mixins for database, file, and HTTP IO
@@ -38,18 +70,46 @@ class BenchmarkReporter:
 
 
 class ConsoleReporter(BenchmarkReporter):
-    # TODO: Implement regex filters, context values, display options, ... (__init__)
-    def report_result(self, record: BenchmarkRecord) -> None:
+    def __init__(self, tablefmt: str = "simple"):
+        self.tablefmt = tablefmt
+
+    def report_result(
+        self,
+        record: BenchmarkRecord,
+        benchmark_filter: str | None = None,
+        include_context: tuple[str, ...] = (),
+        exclude_empty: bool = True,
+    ) -> None:
         try:
             from tabulate import tabulate
         except ModuleNotFoundError:
             raise ValueError(
-                f"{self.__class__.__name__} requires `tabulate` to be installed. "
+                f"class {self.__class__.__name__}() requires `tabulate` to be installed. "
                 f"To install, run `{sys.executable} -m pip install --upgrade tabulate`."
             )
 
-        benchmarks = record["benchmarks"]
-        print(tabulate(benchmarks, headers="keys"))
+        ctx, benchmarks = record["context"], record["benchmarks"]
+
+        nulls = set() if not exclude_empty else nullcols(benchmarks)
+
+        if benchmark_filter is not None:
+            regex = re.compile(benchmark_filter, flags=re.IGNORECASE)
+        else:
+            regex = None
+
+        filtered = []
+        for bm in benchmarks:
+            if regex is not None and regex.search(bm["name"]) is None:
+                continue
+            bm_new = copy.copy(bm)
+            filteredctx = {k: nested_getitem(ctx, k) for k in include_context}
+            bm_new.update(filteredctx)
+            for nc in nulls:
+                bm_new.pop(nc)
+            filtered.append(bm_new)
+
+        # TODO: Add support for custom formatters
+        print(tabulate(filtered, headers="keys", tablefmt=self.tablefmt))
 
 
 # internal, mutable
