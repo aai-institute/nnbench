7240eee7129c5ba6e7b55b8b8c55a4d30b2e3e44
Nicholas Junge <n.junge@appliedai.de>
2024-02-08 10:10:05 +0100

    Fix nullcolumns check, add dirty check to git provider
    This is good for reproducibility analysis.

--- src/nnbench/reporter.py
+++ src/nnbench/reporter.py
@@ -15,11 +15,28 @@ from nnbench.types import BenchmarkRecord
 
 
 def nullcols(_benchmarks: list[dict[str, Any]]) -> tuple[str, ...]:
+    """
+    Extracts columns that only contain false-ish data from a list of benchmarks.
+
+    Since this data is most often not interesting, the result of this
+    can be used to filter out these columns from the benchmark dictionaries.
+
+    Parameters
+    ----------
+    _benchmarks: list[dict[str, Any]]
+        The benchmarks to filter.
+
+    Returns
+    -------
+    tuple[str, ...]
+        Tuple of the columns (key names) that only contain false-ish values
+        across all benchmarks.
+    """
     nulls: dict[str, bool] = collections.defaultdict(bool)
     for bm in _benchmarks:
         for k, v in bm.items():
             nulls[k] = nulls[k] or bool(v)
-    return tuple(k for k, v in nulls.items() if v)
+    return tuple(k for k, v in nulls.items() if not v)
 
 
 def flatten(d: dict[str, Any], prefix: str = "", sep: str = ".") -> dict[str, Any]:
