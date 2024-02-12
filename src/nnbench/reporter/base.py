048ea51e47a3fd904086498e43eb289ad1cb7e1d
Nicholas Junge <n.junge@appliedai.de>
2024-02-12 11:41:20 +0100

    Inline console reporter into `BenchmarkReporter.display()`
    Since the ability to print to console is so nice, we change the console
    reporter to be available on every reporter descendant by adding it as
    the `display()` method on the base.
    
    This means that a) the console reporter file can go, and b) tabulate
    has to be upgraded to an obligatory dependency (at least until we have
    a better tabular formatter in rich etc.).

--- src/nnbench/reporter/base.py
+++ src/nnbench/reporter/base.py
@@ -1,5 +1,9 @@
-from typing import Sequence
+import re
+from typing import Any, Callable, Sequence
 
+from tabulate import tabulate
+
+from nnbench.reporter.util import flatten, nullcols
 from nnbench.types import BenchmarkRecord
 
 
@@ -52,6 +56,14 @@ class BenchmarkReporter:
     merge: bool = False
     """Whether to merge multiple BenchmarkRecords before reporting."""
 
+    def __init__(
+        self,
+        tablefmt: str = "simple",
+        custom_formatters: dict[str, Callable[[Any], Any]] | None = None,
+    ):
+        self.tablefmt = tablefmt
+        self.custom_formatters: dict[str, Callable[[Any], Any]] = custom_formatters or {}
+
     def initialize(self):
         """
         Initialize the reporter's state.
@@ -72,6 +84,42 @@ class BenchmarkReporter:
 
     merge_records = staticmethod(default_merge)
 
+    def display(
+        self,
+        record: BenchmarkRecord,
+        benchmark_filter: str | None = None,
+        include_context: tuple[str, ...] = (),
+        exclude_empty: bool = True,
+    ) -> None:
+        ctx, benchmarks = record["context"], record["benchmarks"]
+
+        nulls = set() if not exclude_empty else nullcols(benchmarks)
+
+        if benchmark_filter is not None:
+            regex = re.compile(benchmark_filter, flags=re.IGNORECASE)
+        else:
+            regex = None
+
+        filtered = []
+        for bm in benchmarks:
+            if regex is not None and regex.search(bm["name"]) is None:
+                continue
+            filteredctx = {
+                k: v
+                for k, v in flatten(ctx).items()
+                if any(k.startswith(i) for i in include_context)
+            }
+            filteredbm = {k: v for k, v in bm.items() if k not in nulls}
+            filteredbm.update(filteredctx)
+            # only apply custom formatters after context merge
+            #  to allow custom formatting of context values.
+            filteredbm = {
+                k: self.custom_formatters.get(k, lambda x: x)(v) for k, v in filteredbm.items()
+            }
+            filtered.append(filteredbm)
+
+        print(tabulate(filtered, headers="keys", tablefmt=self.tablefmt))
+
     def read(self) -> BenchmarkRecord:
         raise NotImplementedError
 
@@ -82,11 +130,6 @@ class BenchmarkReporter:
         raise NotImplementedError
 
     def write_batched(self, records: Sequence[BenchmarkRecord]) -> None:
-        # by default, merge first and then write.
-        if self.merge:
-            merged = self.merge_records(records)
-            self.write(merged)
-        else:
-            # write everything in a loop.
-            for record in records:
-                self.write(record)
+        # By default, just loop over the records and write() everything.
+        for record in records:
+            self.write(record)
