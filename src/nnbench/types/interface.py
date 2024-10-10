847d6dd021e4772940ab5c94f10f0015560f972e
Nicholas Junge <n.junge@appliedai-institute.de>
2024-10-10 20:22:51 +0200

    Switch project to uv lockfile (#148)
    * Add uv lockfile, bump pre-commit hooks
    
    * Remove requirement files
    
    * Switch all actions to uv
    
    Migrated over from lakefs-spec. Everything is as we have it there -
    uv installation, running everything via `uv run`, etc.
    
    The mike setup means that the mike action can only be run after a uv setup.
    
    * Add Python version matrix, fix EOF in pre-commit config
    
    * Check out repo to depth 0 in docs job
    
    * Fix Self import for Python >=3.11

--- src/nnbench/types/interface.py
+++ src/nnbench/types/interface.py
@@ -1,11 +1,15 @@
 """Type interface for the function interface"""
 
 import inspect
+import sys
 from collections.abc import Callable
 from dataclasses import dataclass
 from typing import Any, TypeVar
 
-from typing_extensions import Self
+if sys.version_info >= (3, 11):
+    from typing import Self
+else:
+    from typing_extensions import Self
 
 T = TypeVar("T")
 Variable = tuple[str, type, Any]
