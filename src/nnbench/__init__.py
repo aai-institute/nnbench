90f436150aaff5ca1bca8271c660e0a9ebe198e5
Nicholas Junge <nicholas.junge@web.de>
2024-10-30 19:22:43 +0100

    Remove setuptools_scm in favor of explicit version management
    setuptools_scm has a few drawbacks to version management by hand, such
    as inflexibility for releasing off-tag, and missing info on shallow
    checkouts.
    
    As such, revert back to a static version number, with tagged commits as
    version bumps.

--- src/nnbench/__init__.py
+++ src/nnbench/__init__.py
@@ -1,17 +1,11 @@
 """A framework for organizing and running benchmark workloads on machine learning models."""
 
-from importlib.metadata import PackageNotFoundError, version
-
-try:
-    __version__ = version("nnbench")
-except PackageNotFoundError:
-    # package is not installed
-    pass
-
 from .core import benchmark, parametrize, product
 from .reporter import BenchmarkReporter
 from .runner import BenchmarkRunner
 from .types import Benchmark, BenchmarkRecord, Memo, Parameters
+
+__version__ = "0.3.0"
 
 
 # TODO: This isn't great, make it functional instead?
