ed206925e6d5d9159dcae6b4d1c65328a0b81cfa
Nicholas Junge <n.junge@appliedai-institute.de>
2024-11-27 16:01:08 +0100

    Add TOML config parsing stub
    Contains version-aware stubs for importing a standard-library TOML parser
    for Python 3.10+ (our support matrix).
    
    Next, we will parse out `tool.nnbench` fields, validate their structure,
    and instantiate different types of command classes from there.

---
+++ src/nnbench/config.py
@@ -0,0 +1,13 @@
+import sys
+
+if sys.version_info >= (3, 11):
+    import tomllib
+else:
+    import tomli as tomllib
+
+
+def parse_nnbench_config(pyproject_path):
+    with open(pyproject_path, "rb") as fp:
+        config = tomllib.load(fp)
+
+    return config.get("tool.nnbench", {})
