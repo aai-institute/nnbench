4e6cbcb4887bf200a52d041394c56928d2e26127
Nicholas Junge <n.junge@appliedai-institute.de>
2024-11-27 16:18:22 +0100

    Add type hints to the parse method
    To get an idea of what we want to support, we should get experience with
    running complicated CLI experiments. At minimum, the top-level and "context"
    for custom context providers should be in.

--- src/nnbench/config.py
+++ src/nnbench/config.py
@@ -1,4 +1,6 @@
+import os
 import sys
+from typing import Any
 
 if sys.version_info >= (3, 11):
     import tomllib
@@ -6,7 +8,9 @@ else:
     import tomli as tomllib
 
 
-def parse_nnbench_config(pyproject_path):
+# TODO: It's not just Any in the return, but a few well-defined context keys -
+# put some effort into parsing them
+def parse_nnbench_config(pyproject_path: str | os.PathLike[str]) -> dict[str, Any]:
     with open(pyproject_path, "rb") as fp:
         config = tomllib.load(fp)
 
