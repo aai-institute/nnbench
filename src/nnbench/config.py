360d36d1e5d1e3d83552bf011ab3d0bac4c8951e
Nicholas Junge <n.junge@appliedai-institute.de>
2024-11-28 12:05:52 +0100

    Add autolocator for pyproject.toml, try log-level autofill
    This is a POC for piping the log level through to the CLI. In the first
    local test, setting `log-level = "DEBUG"` in the `[tool.nnbench]` section
    produces the expected debug logs.
    
    Changes the logger gets to the respective module names, so that we get
    all of nnbench's descendant logs.

--- src/nnbench/config.py
+++ src/nnbench/config.py
@@ -1,5 +1,9 @@
+"""Utilities for parsing an nnbench config block out of a pyproject.toml."""
+
+import logging
 import os
 import sys
+from pathlib import Path
 from typing import Any
 
 if sys.version_info >= (3, 11):
@@ -7,11 +11,31 @@ if sys.version_info >= (3, 11):
 else:
     import tomli as tomllib
 
+logger = logging.getLogger("nnbench.config")
+
+
+def locate_pyproject() -> os.PathLike[str]:
+    cwd = Path.cwd()
+    for p in (cwd, *cwd.parents):
+        if (pyproject_cand := (p / "pyproject.toml")).exists():
+            return pyproject_cand
+        if p == Path.home():
+            break
+    raise RuntimeError("could not locate pyproject.toml")
+
 
 # TODO: It's not just Any in the return, but a few well-defined context keys -
 # put some effort into parsing them
-def parse_nnbench_config(pyproject_path: str | os.PathLike[str]) -> dict[str, Any]:
+def parse_nnbench_config(pyproject_path: str | os.PathLike[str] | None = None) -> dict[str, Any]:
+    if pyproject_path is None:
+        try:
+            pyproject_path = locate_pyproject()
+        except RuntimeError:
+            # TODO: This should hold sensible default values for all top-level keys,
+            # preferably as a dataclass.
+            # pyproject.toml cannot be found, so return an empty config.
+            return {"log-level": "NOTSET"}
+
     with open(pyproject_path, "rb") as fp:
         config = tomllib.load(fp)
-
     return config.get("tool", {}).get("nnbench", {})
