8a963e6472808ef78e97e6eb069eb8c30f91d566
Nicholas Junge <nicholas.junge@web.de>
2025-01-20 15:53:23 +0100

    examples: Rewrite HuggingFace example with `@nnbench.parametrize`
    This gives us the same memory efficiency gains we enjoyed before, but in a more
    compact format.
    
    Moves the parameter instantiation (model, valdata, index->label mapping) into the
    runner file.

--- examples/huggingface/runner.py
+++ examples/huggingface/runner.py
@@ -1,10 +1,36 @@
+from collections.abc import Iterable
+
+from datasets import Dataset, load_dataset
+from transformers import (
+    AutoModelForTokenClassification,
+    AutoTokenizer,
+)
+
 import nnbench
+
+dataset = load_dataset("conllpp")
+path = dataset.cache_files["validation"][0]["filename"]
 
 
 def main() -> None:
+    model = AutoModelForTokenClassification.from_pretrained(
+        "dslim/distilbert-NER", use_safetensors=True
+    )
+    tokenizer = AutoTokenizer.from_pretrained("dslim/distilbert-NER")
+    valdata = Dataset.from_file(path)
+    label_names: Iterable[str] = valdata.features["ner_tags"].feature.names
+    index_label_mapping = {i: label for i, label in enumerate(label_names)}
+
+    params = {
+        "model": model,
+        "tokenizer": tokenizer,
+        "valdata": valdata,
+        "index_label_mapping": index_label_mapping,
+    }
+
     benchmarks = nnbench.collect("benchmark.py", tags=("per-class",))
     reporter = nnbench.ConsoleReporter()
-    result = nnbench.run(benchmarks)
+    result = nnbench.run(benchmarks, params=params)
     reporter.display(result)
 
 
