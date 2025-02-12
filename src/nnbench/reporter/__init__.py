72cd485c337ce8e60a1a7d828071b5ce80537344
Nicholas Junge <n.junge@appliedai-institute.de>
2025-02-12 16:23:30 +0100

    Add MLFlowIO to enable MLflow logging of benchmark results
    Introduces a `BenchmarkServiceIO` protocol, establishing an interface for
    streaming JSON results to web services like MLflow, Grafana, WandB, ...

--- src/nnbench/reporter/__init__.py
+++ src/nnbench/reporter/__init__.py
@@ -3,5 +3,39 @@ An interface for displaying, writing, or streaming benchmark results to
 files, databases, or web services.
 """
 
+import os
+from enum import Enum
+from typing import TextIO
+
 from .console import ConsoleReporter
 from .file import FileReporter
+
+
+class IOType(str, Enum):
+    STDOUT = "stdout"
+    FILE = "file"
+    DATABASE = "database"
+    SERVICE = "service"  # TODO: Pick a better name
+    UNKNOWN = "unknown"
+
+
+def get_io_type(uri: str | os.PathLike[str] | TextIO) -> IOType:
+    import sys
+
+    if uri is sys.stdout:
+        return IOType.STDOUT
+
+    from nnbench.util import get_protocol
+
+    proto = get_protocol(uri)
+    if proto in ["az", "file", "gcs", "gs", "lakefs", "s3"]:
+        return IOType.FILE
+    elif proto in ["sqlite"]:  # TODO: Not yet supported
+        return IOType.DATABASE
+    elif proto in ["mlflow"]:
+        return IOType.SERVICE
+    else:
+        return IOType.UNKNOWN
+
+
+del Enum, os, TextIO
