72814db9259ef9048d9e796a3b94b3f99a05ae66
Nicholas Junge <n.junge@appliedai-institute.de>
2025-02-12 13:06:36 +0100

    chore: Remove stale reporter code, switch package to use new IO protocol
    duckdb and the base class were unused. Since we need to employ different
    abstractions for different IO models, a general base class was not that
    useful aside for maybe type safety.

--- src/nnbench/reporter/__init__.py
+++ src/nnbench/reporter/__init__.py
@@ -3,7 +3,5 @@ An interface for displaying, writing, or streaming benchmark results to
 files, databases, or web services.
 """
 
-from .base import BenchmarkReporter
 from .console import ConsoleReporter
-from .duckdb_sql import DuckDBReporter
 from .file import FileReporter
