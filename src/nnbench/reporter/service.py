135698d1d9fb0af412202e7e3104b2c1c3017cc7
Nicholas Junge <n.junge@appliedai-institute.de>
2025-02-20 16:53:01 +0100

    cli: Add query to reporter.read interface, add URI support to compare
    The query field in `IO.read()` allows us to send data queries to database-like
    IO interfaces, such as that of most experiment trackers and databases themselves.
    
    The URI support was taken over verbatim from the already working bit in
    `nnbench run`.

--- src/nnbench/reporter/service.py
+++ src/nnbench/reporter/service.py
@@ -3,7 +3,6 @@ from contextlib import ExitStack
 from pathlib import Path
 from typing import TYPE_CHECKING, Any, Protocol
 
-from nnbench.reporter.util import get_protocol
 from nnbench.types import BenchmarkRecord
 
 if TYPE_CHECKING:
@@ -11,7 +10,9 @@ if TYPE_CHECKING:
 
 
 class BenchmarkServiceIO(Protocol):
-    def read(self, uri: str | os.PathLike[str], options: dict[str, Any]) -> BenchmarkRecord: ...
+    def read(
+        self, uri: str | os.PathLike[str], query: str | None, options: dict[str, Any]
+    ) -> BenchmarkRecord: ...
 
     def write(
         self, record: BenchmarkRecord, uri: str | os.PathLike[str], options: dict[str, Any]
@@ -42,7 +43,9 @@ class MLFlowIO(BenchmarkServiceIO):
         else:
             return self.mlflow.start_run(run_name=run_name, nested=nested)
 
-    def read(self, uri: str | os.PathLike[str], options: dict[str, Any]) -> BenchmarkRecord:
+    def read(
+        self, uri: str | os.PathLike[str], query: str | None, options: dict[str, Any]
+    ) -> BenchmarkRecord:
         raise NotImplementedError
 
     def write(
@@ -52,44 +55,20 @@ class MLFlowIO(BenchmarkServiceIO):
         try:
             experiment, run_name, *subruns = Path(uri).parts
         except ValueError:
-            raise ValueError(f"expected URI of form <experiment>/<run>[/<subrun>]..., got {uri!r}")
+            raise ValueError(f"expected URI of form <experiment>/<run>[/<subrun>...], got {uri!r}")
 
         # setting experiment removes the need for passing the `experiment_id` kwarg
         # in the subsequent API calls.
-        self.mlflow.set_experiment(experiment_name=experiment)
+        self.mlflow.set_experiment(experiment)
 
         run = self.stack.enter_context(self.get_or_create_run(run_name=run_name))
         for s in subruns:
             # reassignment ensures that we log into the max-depth subrun specified.
             run = self.stack.enter_context(self.get_or_create_run(run_name=s, nested=True))
 
-        self.mlflow.log_dict(record.context, "context.json", run_id=run.info.run_id)
+        run_id = run.info.run_id
+        self.mlflow.log_dict(record.context, "context.json", run_id=run_id)
         for bm in record.benchmarks:
             name, value = bm["name"], bm["value"]
             timestamp = bm["timestamp"]
-            self.mlflow.log_metric(name, value, timestamp=timestamp, run_id=run.info.run_id)
-
-
-# TODO: Investigate if this can be merged with the file io mapping to a top-level struct
-#  (e.g. with a value union type BenchmarkFileIO | BenchmarkServiceIO | ...)
-_service_io_mapping: dict[str, type[BenchmarkServiceIO]] = {
-    "mlflow": MLFlowIO,
-}
-
-
-def get_service_io_class(uri: str) -> BenchmarkServiceIO:
-    proto = get_protocol(uri)
-    try:
-        return _service_io_mapping[proto]()
-    except KeyError:
-        raise ValueError(f"unsupported benchmark IO: {proto!r}") from None
-
-
-def register_file_io_class(
-    name: str, klass: type[BenchmarkServiceIO], clobber: bool = False
-) -> None:
-    if name in _service_io_mapping and not clobber:
-        raise RuntimeError(
-            f"IO {name!r} is already registered (to force registration, rerun with clobber=True)"
-        )
-    _service_io_mapping[name] = klass
+            self.mlflow.log_metric(name, value, timestamp=timestamp, run_id=run_id)
