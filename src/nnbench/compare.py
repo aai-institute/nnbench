22f77065ff3e5f25e0d96d5586c1c61d9bf7c1b9
Nicholas Junge <n.junge@appliedai-institute.de>
2025-03-17 12:49:02 +0100

    comparisons: Add abstract base class, comparators, JSON loading for CLI
    To load a comparison config (JSON) on the CLI, pass `--comparison-file=$FILE`,
    where the file points to a JSON containing key-value pairs of metric names and
    comparators.
    
    Fixes the table renderer to inherit from an abstract base, make comparisons
    optional for metrics, and skip comparisons for values which are missing in
    other records.

--- src/nnbench/compare.py
+++ src/nnbench/compare.py
@@ -1,7 +1,7 @@
 """Contains machinery to compare multiple benchmark records side by side."""
 
 import operator
-from collections.abc import Sequence
+from collections.abc import Iterable
 from typing import Any, Protocol
 
 from rich.console import Console
@@ -10,18 +10,49 @@ from rich.table import Table
 from nnbench.types import BenchmarkRecord
 
 _MISSING = "N/A"
+_STATUS_KEY = "Status"
 
 
 class Comparator(Protocol):
     def __call__(self, val1: Any, val2: Any) -> bool: ...
 
 
-# TODO: Add vectorized comparators
-
-
-class gt(Comparator):
+class GreaterEqual(Comparator):
     def __call__(self, val1: Any, val2: Any) -> bool:
-        return operator.gt(val1, val2)
+        return operator.ge(val1, val2)
+
+    def __str__(self):
+        return "x ≥ y"
+
+
+class LessEqual(Comparator):
+    def __call__(self, val1: Any, val2: Any) -> bool:
+        return operator.le(val1, val2)
+
+    def __str__(self):
+        return "x ≤ y"
+
+
+class AbsDiffLessEqual(Comparator):
+    def __init__(self, thresh: float):
+        self.thresh = thresh
+
+    def __call__(self, val1: float, val2: float) -> bool:
+        return operator.le(operator.abs(val1 - val2), self.thresh)
+
+    def __str__(self):
+        return f"|x - y| <= {self.thresh:.2%}"
+
+
+def make_row(record: BenchmarkRecord) -> dict[str, Any]:
+    d = dict()
+    d["run"] = record.run
+    for bm in record.benchmarks:
+        # TODO: Guard against key errors from database queries
+        name, value = bm["function"], bm["value"]
+        d[name] = value
+        # TODO: Check for errors
+    return d
 
 
 def get_value_by_name(record: BenchmarkRecord, name: str, missing: str) -> Any:
@@ -49,7 +80,7 @@ def get_value_by_name(record: BenchmarkRecord, name: str, missing: str) -> Any:
         as rich text.
 
     """
-    metric_names = [b["name"] for b in record.benchmarks]
+    metric_names = [b["function"] for b in record.benchmarks]
     if name not in metric_names:
         return missing
 
@@ -60,20 +91,66 @@ def get_value_by_name(record: BenchmarkRecord, name: str, missing: str) -> Any:
     return res.get("value", missing)
 
 
-class Comparison:
-    def __init__(self, placeholder: str = _MISSING):
+class AbstractComparison:
+    def compare2(self, name: str, val1: Any, val2: Any) -> bool:
+        """A subroutine to compare two values of a metric ``name`` against each other."""
+        raise NotImplementedError
+
+    def render(self) -> None:
+        """A method to render a previously computed comparison to a stream."""
+        raise NotImplementedError
+
+    @property
+    def success(self) -> bool:
         """
-        Initialize a comparison class.
+        Indicates whether a comparison has been succesful based on the criteria
+        expressed by the chosen set of comparators.
+        """
+        raise NotImplementedError
+
+
+class TabularComparison(AbstractComparison):
+    def __init__(
+        self,
+        records: Iterable[BenchmarkRecord],
+        comparators: dict[str, Comparator] | None = None,
+        placeholder: str = _MISSING,
+    ):
+        """
+        Initialize a tabular comparison class, rendering the result to a rich table.
 
         Parameters
         ----------
+        records: Sequence[BenchmarkRecord]
+            The benchmark records to compare.
+        comparators: dict[str, Comparator]
+            A mapping from benchmark functions to comparators, i.e. a function
+            comparing two results and returning a boolean indicating a favourable or
+            unfavourable comparison.
         placeholder: str
             A placeholder string to show in the event of a missing metric.
         """
         self.placeholder = placeholder
-        self.comparisons: list[dict[str, Any]] = []
+        self.comparators = comparators or {}
 
-    def compare2(self, metric_name: str, val1: Any, val2: Any) -> bool:
+        self.records: list[BenchmarkRecord] = list(records)
+        self.data: list[dict[str, Any]] = [make_row(rec) for rec in self.records]
+        self.metrics: list[str] = []
+        self._success: bool = False
+
+        self.display_names: dict[str, str] = {}
+        for rec in self.records:
+            for bm in rec.benchmarks:
+                name, func = bm["name"], bm["function"]
+                if func not in self.display_names:
+                    self.display_names[func] = name
+                if func not in self.metrics:
+                    self.metrics.append(func)
+
+        if len(self.data) < 2:
+            raise ValueError("must give at least two records to compare")
+
+    def compare2(self, name: str, val1: Any, val2: Any) -> bool:
         """
         Compare two values of a metric across runs.
 
@@ -85,7 +162,7 @@ class Comparison:
 
         Parameters
         ----------
-        metric_name: str
+        name: str
             Name of the metric to compare.
         val1: Any
             Value of the metric in the first benchmark record.
@@ -97,55 +174,54 @@ class Comparison:
         bool
             The comparison between the two values.
         """
-        # TODO: Make this abstract for the library user to implement
         if any(v == self.placeholder for v in (val1, val2)):
             return False
-        _name = metric_name.lower()
-        if "accuracy" in _name:
-            return operator.le(val1, val2)
-        else:
-            return operator.abs(val1 - val2) <= 0.01
+        return self.comparators[name](val1, val2)
 
-    def get_comparison(self, metric_name: str) -> str:
-        _name = metric_name.lower()
-        if "accuracy" in _name:
-            return "val1 ≤ val2"
+    def format_value(self, name: str, val: Any) -> str:
+        if val == self.placeholder:
+            return self.placeholder
+        if name == "accuracy":
+            return f"{val:.2%}"
         else:
-            return "|val1 - val2| ≤ 0.01"
+            return f"{val:.2f}"
 
-    def render(self, records: Sequence[BenchmarkRecord]) -> None:
+    def render(self) -> None:
         """
         Compare a series of benchmark records, displaying their results in a table
         side by side.
-
-        Parameters
-        ----------
-        records: Sequence[BenchmarkRecord]
-            The benchmark records to compare.
         """
-        if len(records) < 2:
-            raise ValueError("must give at least two records to compare")
-
+        c = Console()
         t = Table()
 
-        runs = [rec.run for rec in records]
-        rows: list[list[str]] = []
-        columns: list[str] = ["Metric Name"] + runs + ["Comparison", "Result"]
+        if self.comparators:
+            c.print("Comparison strategy: All vs. first")
+            c.print("Comparisons:")
+            for k, v in self.comparators.items():
+                c.print(f"    {k}: {v}")
 
-        metrics = [(bm["name"], bm["value"]) for bm in records[0].benchmarks]
-        # Main loop, extracts values from the individual records,
-        # or a placeholder if there are any.
-        for metric in metrics:
-            name, val = metric
+        rows: list[list[str]] = []
+        columns: list[str] = ["Run Name"] + list(self.display_names.values())
+        if set(self.metrics) | self.comparators.keys():
+            columns += ["Status"]
+
+        for i, d in enumerate(self.data):
+            row = [d["run"]]
             status = ""
-            comparison = self.get_comparison(name)
-            row = [name, str(val)]
-            for record in records[1:]:
-                compval = get_value_by_name(record, name, self.placeholder)
-                success = self.compare2(name, val, compval)
-                status += ":white_check_mark:" if success else ":x:"
-                row += [str(compval)]
-            row += [comparison, status]
+            for metric in self.metrics:
+                val = d.get(metric, self.placeholder)
+                sval = self.format_value(metric, val)
+                comparator = self.comparators.get(metric, None)
+                if i and comparator is not None:
+                    cd = self.data[0]
+                    compval = cd.get(metric, self.placeholder)
+                    success = self.compare2(metric, val, compval)
+                    self._success |= success
+                    status += ":white_check_mark:" if success else ":x:"
+                    sval += " (vs. " + self.format_value(metric, compval) + ")"
+                row += [sval]
+            if "Status" in columns:
+                row += [status]
             rows.append(row)
 
         for column in columns:
@@ -153,10 +229,9 @@ class Comparison:
         for row in rows:
             t.add_row(*row)
 
-        c = Console()
         c.print(t)
 
     @property
     def success(self) -> bool:
         """Indicates if the current comparison was successful."""
-        return True
+        return self._success
