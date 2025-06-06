9a4e37cd7fa99178a2dab747bbc5b153ac1cd42a
Nicholas Junge <nicholas.junge@web.de>
2025-06-06 11:31:45 +0200

    Bump version to v0.5.0

--- src/nnbench/__init__.py
+++ src/nnbench/__init__.py
@@ -4,4 +4,4 @@ from .core import benchmark, parametrize, product
 from .runner import collect, run
 from .types import Benchmark, BenchmarkFamily, BenchmarkResult, Parameters
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
